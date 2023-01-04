4988a48384eafe6435347afaba2df1b5a8a26902
KhunPat21 <elfmaza007@gmail.com>
2023-01-04 16:47:25 +0700

    update

---
+++ week1202.php
@@ -0,0 +1,17 @@
+<?php
+ฟังก์ชัน day_name (){
+    $ day1 = "วันจันทร์ ";
+    $ day2 = "วันอังคาร ";
+    $ day3 = "วันพุธ ";
+
+   // return [$day1,$day2,$day3];
+    อาร์เรย์ส่งคืน ( $ day1 , $ day2 , $ day3 );
+}
+//$days = day_name();
+//list($day1,$day2,$day3) = day_name();
+รายการ ( $ day_1 , $ day_2 , $ day_3 ) = day_name();
+//print($days[0].','.$days[1].','.$days[2]);
+//print($day1.','.$day2.','.$day3);
+พิมพ์( $ day_1 . ',' . $ day_2 . ',' . $ day_3 );
+
+?>
